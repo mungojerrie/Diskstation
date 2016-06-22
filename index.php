af078705771f262f1706b998d109d04ffb0f1dcf
Jan <Jan@192.168.10.30>
2016-06-22 21:47:32 +0200

    cv

--- index.php
+++ index.php
@@ -1,20 +1,257 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        foreach ($array as $key => $value) {
-            
-        }
-        ?>
-    </body>
+   <head>
+      <title>Jan Gravgaard Resume | Resume Builder</title>
+      <link href="/web_css/white.css" media="all" rel="stylesheet" type="text/css" />
+      <meta name="description" content="" />
+      <meta name="keywords" content="Curriculum Vitae" />
+      <link rel="icon" href="/favicon.ico" type="image/ico" />
+      <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
+      <meta name="robots" content="noindex, nofollow" />
+      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+   </head>
+   <body>
+      <div id="wrapper" >
+         <!-- -->
+         <div id="view">
+            <div id="resume">
+               <div class="header"></div>
+               <div class="section picture">
+                  <div class="content"><img alt="Profile Picture" src="/web_images/jg_small.jpg" /></div>
+                  <div class="clear"></div>
+               </div>
+               <h1 class="name">
+                  <span class="first-name">Jan</span>
+                  <span class="last-name">Gravgaard</span>
+                  <span class="headline">Systemudvikler at Nordisk Dæk Import A/S</span>
+                  <div class="clear"></div>
+               </h1>
+               <div class="section contact">
+                  <h2 class="section-header">
+                     <span class="text">Contact</span>	
+                  </h2>
+                  <div class="content">
+                     <div class="main-address">
+                        <span class="label">Address:</span>
+                        <span class="content">Jarlsvej 19, Jels, 6630 Rødding</span>
+                     </div>
+                     <div class="phone-home phone-number">
+                        <span class="label">Home:</span>
+                        <span class="content">23829955</span>
+                     </div>
+                  </div>
+                  <div class="clear"></div>
+               </div>
+               <div class="section summary">
+                  <h2 class="section-header">
+                     <span class="text">Summary</span>	
+                  </h2>
+                  <div class="content">
+                     <p>Professionelt er jeg analytisk stærk, grundig, systematisk og kvalitetsbevidst. Jeg er en social person der trives godt med team arbejde, jeg vægter den gode tone og humor højt. Stærk og ligefrem i kommunikationen med både mine kollegaer og kunder – tingene bliver sagt lige ud på en fair og ordentlig måde. 
+                        Jeg er vellidt blandt kollegaer og altid parat til at tage en ekstra tørn hvis behov for det. Mit fokus er altid at levere god kvalitet til kunderne – tingene skal være i orden første gang. Ydermere formår jeg i mit arbejde at have øje for detaljer, samtidig med jeg evner at bevare overblikket.
+                     </p>
+                  </div>
+                  <div class="clear"></div>
+               </div>
+               <div class="section skills">
+                  <h2 class="section-header">
+                     <span class="text">Skills</span>	
+                  </h2>
+                  <div class="content">
+                     <ul class="skills-list">
+                        <li class="skill first">
+                           <span class="skill-name">Visual C#</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">T-SQL</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">XML</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">PHP</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">Java</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">Software Development</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">C#</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">Microsoft SQL Server</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">.NET</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">SQL Tuning</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">Query Tuning</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">Performance Tuning</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">SQL</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">Database Design</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">Visual Studio</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">Design Patterns</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">UML</span>
+                        </li>
+                        <li class="skill">
+                           <span class="skill-name">IIS</span>
+                        </li>
+                     </ul>
+                  </div>
+                  <div class="clear"></div>
+               </div>
+               <div class="section positions">
+                  <h2 class="section-header">
+                     <span class="text">Experience</span>	
+                  </h2>
+                  <div class="content">
+                     <div class="position first">
+                        <div class="company-name">Nordisk Dæk Import A/S</div>
+                        <div class="dates">
+                           <span class="start-month">02 /</span>
+                           <span class="start-year">2016</span>
+                           <span class="date-separator">-</span>
+                           <span class="is-current">Present</span>
+                        </div>
+                        <div class="title">Systemudvikler</div>
+                        <div class="job-summary">
+                           <p>Optimizing and extending backend system and webshop</p>
+                        </div>
+                     </div>
+                     <div class="position">
+                        <div class="company-name">CSC Scandihealth A/S</div>
+                        <div class="dates">
+                           <span class="start-month">10 /</span>
+                           <span class="start-year">2005</span>
+                           <span class="date-separator">-</span>
+                           <span class="end-month">12 /</span>
+                           <span class="end-year">2015</span>
+                        </div>
+                        <div class="title">Sr Prof: Programmer Analyst</div>
+                        <div class="job-summary">
+                           <p>•	Optimering af kode, C# og T-SQL
+                              •	Fejlfinding og deployment
+                              •	Videreudvikling af kodebasen og snitflader.
+                              •	2. ledssupport og slutbruger kontakt
+                              •	Integration til mange 3. partssnitflader
+                           </p>
+                           <p>Resultater: Optimering og sikring af gode svartider i CSC Vitae Suite. Konvertering, udvikling og samkøring af data ifm. kommune-sammenlægning.</p>
+                        </div>
+                     </div>
+                     <div class="position">
+                        <div class="company-name">Evikali A/S</div>
+                        <div class="dates">
+                           <span class="start-month">10 /</span>
+                           <span class="start-year">2004</span>
+                           <span class="date-separator">-</span>
+                           <span class="end-month">10 /</span>
+                           <span class="end-year">2005</span>
+                        </div>
+                        <div class="title">Java Developer</div>
+                        <div class="job-summary">
+                           <p>•	IT Support ad hoc opgaver
+                              •	Videreudvikling af hjemmesider
+                              •	Installation og opsætning af hardware
+                           </p>
+                           <p>Resultater: Videreudviklede SMS system. Designede og imple- menterede system til OIO XML fakturering</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="clear"></div>
+               </div>
+               <div class="section educations">
+                  <h2 class="section-header">
+                     <span class="text">Education</span>	
+                  </h2>
+                  <div class="content">
+                     <div class="education first">
+                        <div class="school-name">Aarhus Business School</div>
+                        <div class="dates">
+                           <span class="start-year">2000</span>
+                           <span class="date-separator">-</span>
+                           <span class="end-year">2002</span>
+                        </div>
+                        <div class="program">
+                           <span class="degree">Datamatiker</span>
+                           <span class="program-separator">,</span>
+                           <span class="field-of-study">Web development</span>
+                        </div>
+                        <div class="activities">
+                           <p>I was involved in designing and creating a website suited for coach/instructors helping them in planning/creating plans for a given session and the ability to share their work/ideas with other coach/instructors</p>
+                        </div>
+                     </div>
+                     <div class="education">
+                        <div class="school-name">Kolding købmandsskole, IBA</div>
+                        <div class="dates">
+                           <span class="start-year">1996</span>
+                           <span class="date-separator">-</span>
+                           <span class="end-year">1998</span>
+                        </div>
+                        <div class="program">
+                           <span class="degree">Akademi Økonom</span>
+                           <span class="program-separator">,</span>
+                           <span class="field-of-study">International Business</span>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="clear"></div>
+               </div>
+               <div class="section interests">
+                  <h2 class="section-header">
+                     <span class="text">Interests</span>	
+                  </h2>
+                  <div class="content">
+                     <p>Beachvolley, Running, MTB, GDS</p>
+                  </div>
+                  <div class="clear"></div>
+               </div>
+               <div class="section languages">
+                  <h2 class="section-header">
+                     <span class="text">Languages</span>	
+                  </h2>
+                  <div class="content">
+                     <ul class="languages-list">
+                        <li class="language first">
+                           <span class="language-name">English</span>
+                           <span class="language-attributes">
+                           <span class="left-paren">(</span>
+                           <span class="proficiency">Professional working proficiency</span>
+                           <span class="right-paren">)</span>
+                           </span>
+                        </li>
+                        <li class="language">
+                           <span class="language-name">Danish</span>
+                           <span class="language-attributes">
+                           <span class="left-paren">(</span>
+                           <span class="proficiency">Native or bilingual proficiency</span>
+                           <span class="right-paren">)</span>
+                           </span>
+                        </li>
+                     </ul>
+                  </div>
+                  <div class="clear"></div>
+               </div>
+               <div class="footer"></div>
+            </div>
+            <div class="view-clear"></div>
+         </div>
+      </div>
+   </body>
 </html>
